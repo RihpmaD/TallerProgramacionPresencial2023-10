978b9433ec92cb86f25f98320f47d79a327fd01f
BILLY VICTOR CASO BALBIN <129817195+RihpmaD@users.noreply.github.com>
2023-06-14 17:44:42 -0500

    Add files via upload

---
+++ Semana12Ejercicio01/src/semana12ejercicio01/Automovil.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package semana12ejercicio01;
+
+/**
+ *
+ * @author C20177
+ */
+public class Automovil {
+    
+}
